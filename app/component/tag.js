b38c95779e18497d9a435abb9f9b509b5e297771
0xdblok <dblok0x@gmail.com>
2024-04-24 13:47:24 +0200

    ee

--- app/component/tag.js
+++ app/component/tag.js
@@ -11,7 +11,7 @@ export default function Tag() {
       <h2 class="flex flex-row flex-nowrap items-center mt-24">
         <span class="flex-grow block border-t border-black"></span>
         <span class="flex-none block mx-4 px-4 py-2.5 text-md rounded leading-none font-medium bg-black text-white">
-          Aristotle Tags
+          Aristotle Partnerships
         </span>
         <span class="flex-grow block border-t border-black"></span>
       </h2>
